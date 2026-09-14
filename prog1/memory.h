15b2f84d2aec407e5995f2e41ac7121c048f04f6
trtyl <dasherhp23@gmail.com>
2026-09-14 13:26:29 -0500

    back to normal

--- prog1/memory.h
+++ prog1/memory.h
@@ -1,19 +1,10 @@
-// CSC 351 Team 2 - Fall 2026
-// Template written by Josiah W. / Dylan P.
+@ -2,21 +2,41 @@
+// Template written by Josiah W.
 
 
-class node {
-	private:
-		int pid; // -1 is free node, >= 0 is node with PID of that number
-		unsigned int start; // starting memory block id
-		unsigned int length; // number of blocks represented by this node
-		node *prev; // Left pointer, if first it's null
-		node *next; // Right pointer, if last it's null
-	
-	public:
-		node(int pid, unsigned int start, unsigned int length);
-
-};
+// forward declarations so we can define memManager first
+class node;
+class list;
 
 class memManager {
 	private:
@@ -48,20 +39,28 @@ class memManager {
         
         // Prints all the nodes' attributes in the DLL, one node per line
         void printIt();
+};
+
+class node {
+	private:
+		int pid; // -1 is free node, >= 0 is node with PID of that number
+		unsigned int start; // starting memory block id
+		unsigned int length; // number of blocks represented by this node
+		node *prev; // Left pointer, if first it's null
+		node *next; // Right pointer, if last it's null
+		unsigned int start, length; // tracks memory blocks
+		node *prev, *next; // DLL left/right pointers
+	
+	public:
+		node(int pid, unsigned int start, unsigned int length);
+		node(int pid, unsigned int start, unsigned int length,
+						node *prev = NULL, node *next = NULL);
+
+	friend memManager; // Friends are ones who can access your private members
+};
+class memManager {
 
 };
 class list {
 	private:
 		unsigned int *a; // actual contents of the list
-		unsigned int listCount;
-		unsigned int listCapacity; 
-	public:
-		list(unsigned int listCapacity); // constructor
-		~list(); // destructor
-		bool add(int val); // append the list with val: this is constant time
-		bool deleteAt(unsigned int index); // like P1 list delAt, rc is success
-		int readAt(unsigned int index); // read the val at index, otherwise -1
-		void printIt() const; // print list, one entry per line; 4 debug
-		unsigned int getCount(); // return listCount
-
-};
