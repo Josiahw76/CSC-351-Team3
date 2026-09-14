12ef7f755fbd85fa1d6c8d66127504ad86cf93e7
Josiahw76 <josiahaiden76@gmail.com>
2026-09-14 12:24:18 -0500

    created memory files

---
+++ prog1/memory.cpp
@@ -0,0 +1,28 @@
+#include "memory.h"
+
+
+//******************************************************************************
+//				List Functions				      //
+//******************************************************************************
+
+list::list() {}
+
+
+list::~list() {}
+
+
+bool list::add(int val) {}
+
+
+bool list::deleteAt(unsigned int index) {}
+
+
+int list::readAt(unsigned int index) {}
+
+
+void list::printIt() {}
+
+
+unsigned int list::getCount() {}
+
+//******************************************************************************
--- prog1/memory.h
+++ prog1/memory.h
@@ -1,6 +1,22 @@
 // CSC 351 Team 2 - Fall 2026
 // Template written by Josiah W.
 
+
+class node {
+	private:
+		int pid; // -1 is free node, >= 0 is node with PID of that number
+		unsigned int start; // starting memory block id
+		unsigned int length; // number of blocks represented by this node
+		node *prev; // Left pointer, if first it's null
+		node *next; // Right pointer, if last it's null
+	
+	public:
+		node(int pid, unsigned int start, unsigned int length);
+
+};
+class memManager {
+
+};
 class list {
 	private:
 		unsigned int *a; // actual contents of the list
@@ -14,4 +30,5 @@ class list {
 		int readAt(unsigned int index); // read the val at index, otherwise -1
 		void printIt() const; // print list, one entry per line; 4 debug
 		unsigned int getCount(); // return listCount
+
 };
